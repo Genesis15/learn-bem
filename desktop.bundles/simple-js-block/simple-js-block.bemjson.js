46b0285a57a60b6ce4c03ce5463629a28793f46a
Genesis <genesis015@mail.ru>
2015-11-04 15:08:33 +0600

    Урок 1. Простой JS блок

---
+++ desktop.bundles/simple-js-block/simple-js-block.bemjson.js
@@ -0,0 +1,31 @@
+module.exports = {
+    block : 'page',
+    title : 'Title of the page',
+    favicon : '/favicon.ico',
+    head : [
+        { elem : 'meta', attrs : { name : 'description', content : '' } },
+        { elem : 'meta', attrs : { name : 'viewport', content : 'width=device-width, initial-scale=1' } },
+        { elem : 'css', url : 'index.min.css' }
+    ],
+    scripts: [{ elem : 'js', url : 'index.min.js' }],
+    mods : { theme : 'islands' },
+    content : [
+        {
+            block : 'header',
+            content : [
+                'header content goes here'
+            ]
+        },
+        {
+            block: 'simple-js-block',
+            js: true,
+            content: 'This is simple-js-block',
+        },
+        {
+            block : 'footer',
+            content : [
+                'footer content goes here'
+            ]
+        }
+    ]
+};
